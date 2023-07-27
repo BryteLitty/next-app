033e1fea4b47eff658651a559965a06f5090de25
Bright Kportiklah <littyofficials@gmail.com>
2023-07-27 23:28:15 +0000

    add repo details

--- app/code/repos/[name]/page.jsx
+++ app/code/repos/[name]/page.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import Link from 'next/link';
+import { Suspense } from 'react';
 import Repo from '@/components/Repo';
+import RepoDirs from '@/components/RepoDirs';
+
 
 const RepoPage = ({ params: {name} }) => {
   return (
     <div className='card'>
-        <Repo name={name} />
+      <Link href='/code/repos' className='btn btn-button'>
+        Back to Repositories
+      </Link>
+        <Suspense fallback={<div>Loading repo</div>}>
+          <Repo name={name} />
+        </Suspense>
+        <Suspense fallback={<div>Loading repo</div>}>
+          <RepoDirs name={name} />
+        </Suspense>
     </div>
   )
 } 
--- components/Repo.jsx
+++ components/Repo.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import { FaStar, FaCodeBranch, FaEye } from 'react-icons/fa';
 
 async function fetchRepos(name) {
-    const response = await fetch(`https://api.github.com/users/repos/BryteLitty/${name}`);
+    const response = await fetch(`https://api.github.com/repos/BryteLitty/${name}`);
     const repo = await response.json();
     return repo;
 }
 
 const Repo = async ({ name }) => {
     const repo = await fetchRepos(name);
-    console.log(repo)
+    console.log(repo.name)
 
     return (
-        <div>Repo</div>
+        <>
+            <h2>{repo.name}</h2>
+            <p>{repo.description}</p>
+            <div className="card-stats">
+                <div className="card-stat">
+                    <FaStar />
+                    <span>{repo.stargazers_count}</span>
+                </div>
+                <div className="card-stat">
+                    <FaCodeBranch />
+                    <span>{repo.forks_count}</span>
+                </div>
+                <div className="card-stat">
+                    <FaEye />
+                    <span>{repo.watchers_count}</span>
+                </div>
+            </div>
+        </>
     )
 }
 
